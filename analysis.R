f79025023f8228bfc04e2cf563cf75da7fab3692
Alvin Tran <vietra@uw.edu>
2018-05-12 13:15:07 -0700

    changed title

--- analysis.R
+++ analysis.R
@@ -4,7 +4,7 @@ library(dplyr)
 library(stringr)
 source("api-key.R")
 
-username <- "pewdiepie"
+username <- "buzzfeedvideo"
 base_url <- "https://www.googleapis.com/youtube/v3/"
 resource <- "channels?part=snippet&forUsername="
 getid_uri <- paste0(base_url, resource, username,"&key=", google_key)
@@ -15,6 +15,8 @@ getid_df <- flatten(getid_body$items)
 user_id <- flatten(getid_body$items$snippet)
 username_id <- user_id$title
 id <- getid_df$id
+description <- user_id$description
+thumbnail <- paste0("![](", user_id$thumbnails.default.url, ")")
 
 youtuber <- GET(paste0("https://www.googleapis.com/youtube/v3/channels?part=statistics&id=", id, "&key=AIzaSyDH51AIBU564Zem39EZ3KRQhkwKjXil8Io"))
 youtuber_content <- content(youtuber, "text")
